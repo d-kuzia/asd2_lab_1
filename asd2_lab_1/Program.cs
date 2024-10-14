c861fb45d62b3f4548c6e4cb2d04393ad58aaf20
Dania <kuziambaa@gmail.com>
2024-10-14 20:45:32 +0300

    Add project files.

---
+++ asd2_lab_1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+class ExternalSort
+{
+    static void Main(string[] args)
+    {
+        string filePath = "random_nums.txt";
+        int fileSizeMB = 10;
+        int arraySize = (fileSizeMB * 1024 * 1024) / sizeof(int);
+
+        GenerateRandomFile(filePath, arraySize);
+        Console.WriteLine("File with random nums has been created");
+    }
+}
