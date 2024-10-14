4b6238f05a490c13e963c4823d9218532f3d4984
Dania <kuziambaa@gmail.com>
2024-10-14 21:29:38 +0300

    start, non modified done

--- asd2_lab_1/Program.cs
+++ asd2_lab_1/Program.cs
@@ -1,15 +1,140 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
+using System.Collections.Generic;
 
 class ExternalSort
 {
     static void Main(string[] args)
     {
-        string filePath = "random_nums.txt";
+        string inputFilePath = "random_numbers.txt";
+        string tempFolder = "temp";
+        string outputFilePath = "sorted_numbers.txt";
         int fileSizeMB = 10;
+        int chunkSizeMB = 100;
         int arraySize = (fileSizeMB * 1024 * 1024) / sizeof(int);
+        int chunkSize = (chunkSizeMB * 1024 * 1024) / sizeof(int);
 
-        GenerateRandomFile(filePath, arraySize);
-        Console.WriteLine("File with random nums has been created");
+        Directory.CreateDirectory(tempFolder);
+
+        // Generate file with random nums
+        GenerateRandomFile(inputFilePath, arraySize);
+        Console.WriteLine("File with random nums has been created.");
+
+        // Create and sort chunks
+        CreateSortedChunks(inputFilePath, tempFolder, chunkSize);
+        Console.WriteLine("Chunks have been created and sorted.");
+
+        // Merging chunks to output file
+        MultiWayMerge(tempFolder, outputFilePath);
+        Console.WriteLine("File has been sorted.");
+
+        /*using (BinaryReader reader = new BinaryReader(File.Open(outputFilePath, FileMode.Open)))
+        {
+            while (reader.BaseStream.Position < reader.BaseStream.Length)
+            {
+                int number = reader.ReadInt32();
+                Console.WriteLine(number);
+            }
+        }*/
+    }
+
+    static void GenerateRandomFile(string filePath, int count)
+    {
+        Random rand = new Random();
+        using (BinaryWriter writer = new BinaryWriter(File.Open(filePath, FileMode.Create)))
+        {
+            for (int i = 0; i < count; i++)
+            {
+                int randomNumber = rand.Next();
+                writer.Write(randomNumber);
+            }
+        }
+    }
+
+    static void CreateSortedChunks(string inputFilePath, string tempFolder, int chunkSize)
+    {
+        using (BinaryReader reader = new BinaryReader(File.Open(inputFilePath, FileMode.Open)))
+        {
+            int chunkIndex = 0;
+            while (reader.BaseStream.Position < reader.BaseStream.Length)
+            {
+                // Read chunk
+                int[] chunk = new int[Math.Min(chunkSize, (int)(reader.BaseStream.Length - reader.BaseStream.Position) / sizeof(int))];
+                for (int i = 0; i < chunk.Length; i++)
+                {
+                    chunk[i] = reader.ReadInt32();
+                }
+
+                Array.Sort(chunk);
+
+                // Sorted chunk to temp file
+                string tempFilePath = Path.Combine(tempFolder, $"chunk_{chunkIndex}.bin");
+                using (BinaryWriter writer = new BinaryWriter(File.Open(tempFilePath, FileMode.Create)))
+                {
+                    foreach (int number in chunk)
+                    {
+                        writer.Write(number);
+                    }
+                }
+                chunkIndex++;
+            }
+        }
+    }
+
+    static void MultiWayMerge(string tempFolder, string outputFilePath)
+    {
+        string[] tempFiles = Directory.GetFiles(tempFolder, "chunk_*.bin");
+        int k = tempFiles.Length;
+
+        List<BinaryReader> readers = new List<BinaryReader>();
+        using (BinaryWriter writer = new BinaryWriter(File.Open(outputFilePath, FileMode.Create)))
+        {
+            // Open all temps to reaD
+            foreach (string file in tempFiles)
+            {
+                readers.Add(new BinaryReader(File.Open(file, FileMode.Open)));
+            }
+
+            // Priority queue for merging
+            SortedSet<Tuple<int, int>> priorityQueue = new SortedSet<Tuple<int, int>>(Comparer<Tuple<int, int>>.Create((x, y) =>
+            {
+                int result = x.Item1.CompareTo(y.Item1);
+                return result != 0 ? result : x.Item2.CompareTo(y.Item2);
+            }));
+
+            // Read first nums to add to priority queue
+            for (int i = 0; i < readers.Count; i++)
+            {
+                if (readers[i].BaseStream.Position < readers[i].BaseStream.Length)
+                {
+                    int number = readers[i].ReadInt32();
+                    priorityQueue.Add(Tuple.Create(number, i));
+                }
+            }
+
+            // Merging
+            while (priorityQueue.Count > 0)
+            {
+                var smallest = priorityQueue.Min;
+                priorityQueue.Remove(smallest);
+
+                // Smallest to output file
+                writer.Write(smallest.Item1);
+
+                int readerIndex = smallest.Item2;
+                if (readers[readerIndex].BaseStream.Position < readers[readerIndex].BaseStream.Length)
+                {
+                    int nextNumber = readers[readerIndex].ReadInt32();
+                    priorityQueue.Add(Tuple.Create(nextNumber, readerIndex));
+                }
+            }
+
+            // Close readers
+            foreach (var reader in readers)
+            {
+                reader.Close();
+            }
+        }
     }
 }
